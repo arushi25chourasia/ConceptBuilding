3fea6f88e682073ae51eac2a2b6de2fffa299bce
arushi25chourasia <arushi.25chourasia@gmail.com>
2025-07-23 19:03:49 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { Text, SafeAreaView } from 'react-native';
+import React , { useState } from 'react';
+import { Text, SafeAreaView, Button} from 'react-native';
 import FetchUsers from './RenderUsers/FetchUsers';
+import DisplayPosts from './ReactConcepts/FlatListConcept/components/DisplayPosts';
 function App() {
+
+  const [showFetchUsers, setShowFetchUsers] = useState(false);
+  const [displayPosts, setDisplayPosts] = useState(false);
 
   return (
     <SafeAreaView>
-     <Text>hello World</Text>
+     <Text>hell World</Text>
+     <Button title={'Load List'} onPress={() => setShowFetchUsers(true)}/>
+     <Button title={'Display Posts'} onPress={() => setDisplayPosts(true)}/>
      {/**Lists */}
-     <FetchUsers />         
+     {
+      showFetchUsers && <FetchUsers />  
+     }
+     {
+      displayPosts && <DisplayPosts />
+     }
+          
+     
     </SafeAreaView>
   )
 }
